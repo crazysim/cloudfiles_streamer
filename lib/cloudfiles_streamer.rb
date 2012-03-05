4fe916d8279a88a1ef125f7497a6ede3d5303ae5
Andrew Le <andrew@andrewdle.com>
2012-03-05 14:09:22 -0800

    Require cloudfiles streamer files

--- lib/cloudfiles_streamer.rb
+++ lib/cloudfiles_streamer.rb
@@ -1,3 +1,8 @@
+require "cloudfiles_streamer/cloudfiles_api"
+require "cloudfiles_streamer/command_line_options"
+require "cloudfiles_streamer/segmented_stream"
+require "cloudfiles_streamer/streamer"
+
 module CloudFilesStreamer
   VERSION = '0.1.0'
 end
